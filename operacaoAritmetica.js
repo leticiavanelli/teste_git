1e3394eef1e8d0edfe4c499bd79f8af5eb827098
leticia <leticia_vanelli@estudante.sesisenai.org.br>
2023-12-01 11:21:53 -0300

    primeira versão

---
+++ operacaoAritmetica.js
@@ -0,0 +1,26 @@
+let res = document.getElementById('res')
+
+function somar(){
+    let num1 = Number(document.getElementById('num1').value)
+    let num2 = Number(document.getElementById('num2').value)
+
+    let resultadoSoma = num1 + num2
+
+    res.innerHTML = ' A soma de ' + num1 + ' com' + num2 + ' é ' + resultadoSoma.toFixed(2)
+
+}
+
+function diminuir(){
+
+}
+
+
+function multiplicar(){
+
+
+}
+
+function dividir(){
+
+}
+
